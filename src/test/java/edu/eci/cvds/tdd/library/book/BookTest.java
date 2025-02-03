0e2531ae4e49ddac421c534e2570b4d1f4003952
Sebastian Galvis Briceño <sebastian.galvis-b@mail.escuelaing.edu.co>
2025-02-03 12:10:59 -0500

    actualizacion develop

--- src/test/java/edu/eci/cvds/tdd/library/book/BookTest.java
+++ src/test/java/edu/eci/cvds/tdd/library/book/BookTest.java
@@ -1,5 +1,14 @@
 package edu.eci.cvds.tdd.library.book;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.*;
 public class BookTest {
+
+
+    @Test
+    public void getTittle(){
+       assertEquals(2, 2);
+    }
 
 }
