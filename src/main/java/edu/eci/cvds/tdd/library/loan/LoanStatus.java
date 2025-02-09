ffcdec82f92f65097704c16cc2403e701edaa0f5
Sebastian Galvis Briceño <sebastian.galvis-b@mail.escuelaing.edu.co>
2025-02-09 17:38:40 -0500

    Cobertura de Loan

--- src/main/java/edu/eci/cvds/tdd/library/loan/LoanStatus.java
+++ src/main/java/edu/eci/cvds/tdd/library/loan/LoanStatus.java
@@ -2,4 +2,5 @@ package edu.eci.cvds.tdd.library.loan;
 
 
 public enum LoanStatus {
+    ACTIVE, RETURNED
 }
--- src/test/java/edu/eci/cvds/tdd/library/loan/LoanTest.java
+++ src/test/java/edu/eci/cvds/tdd/library/loan/LoanTest.java
@@ -3,11 +3,32 @@ package edu.eci.cvds.tdd.library.loan;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
+import edu.eci.cvds.tdd.library.book.Book;
+import edu.eci.cvds.tdd.library.user.User;
+import java.time.LocalDateTime;
+
 public class LoanTest {
 
     @Test
-    public void getTittle(){
-       assertEquals(2, 2);
+    public void shouldSetAndKnowHisOwnParameters(){
+        Book loanBook = new Book("Las aventuras de Sherlock Holmes", "Arthur Conan Doyle", "isbnX");
+        User loanUser = new User();
+        LocalDateTime loanDate = LocalDateTime.of(2025, 2, 9, 12, 0, 0);
+        LoanStatus loanStatus = LoanStatus.ACTIVE;
+        LocalDateTime loanEndDate = LocalDateTime.of(2025, 3, 9, 12, 0, 0);
+        Loan loan = new Loan();
+
+        loan.setBook(loanBook);
+        loan.setUser(loanUser);
+        loan.setLoanDate(loanDate);
+        loan.setStatus(loanStatus);
+        loan.setReturnDate(loanEndDate);
+
+        assertEquals(loanBook, loan.getBook());
+        assertEquals(loanUser, loan.getUser());
+        assertEquals(loanDate, loan.getLoanDate());
+        assertEquals(loanStatus, loan.getStatus());
+        assertEquals(loanEndDate, loan.getReturnDate());
     }
-    
+
 }
