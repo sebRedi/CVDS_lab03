1b7c3819577507df6f9882473694b59c9ae343e3
Julian Cardenas <julian.cardenas-c@mail.escuelaing.edu.co>
2025-02-09 18:01:55 -0500

    User Test

--- src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
+++ src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -10,4 +10,6 @@ public class LibraryTest {
         assertTrue(true);
     }
 
+
+
 }
--- src/test/java/edu/eci/cvds/tdd/library/user/UserTest.java
+++ src/test/java/edu/eci/cvds/tdd/library/user/UserTest.java
@@ -1,4 +1,23 @@
 package edu.eci.cvds.tdd.library.user;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class UserTest {
+
+    @Test
+    public void shouldReturnTheName() {
+        String name = "julian";
+        User user = new User();
+        user.setName(name);
+        assertEquals(name, user.getName());
+    }
+
+    public void shouldReturnTheId() {
+        String id = "123456";
+        User user = new User();
+        user.setId(id);
+        assertEquals(id, user.getId());
+    }
 }
