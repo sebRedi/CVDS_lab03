ef7d4dbe2c6037bb0c9598aef337c8535255e80b
Julian Cardenas <julian.cardenas-c@mail.escuelaing.edu.co>
2025-02-09 20:18:44 -0500

    addBookTest

--- src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
+++ src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -1,15 +1,27 @@
 package edu.eci.cvds.tdd.library;
 
-import static org.junit.jupiter.api.Assertions.*;
+import edu.eci.cvds.tdd.library.book.Book;
 import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LibraryTest {
 
     @Test
-    public void standardTest(){
-        assertTrue(true);
-    }
+    public void shouldIncreaseTheAmountInAddBook(){
+        Book testBook = new Book("100 años de soledad", "Gabriel Garcia", "1000");
+        Library library = new Library();
+        library.addBook(testBook);
+        assertEquals(1,library.books().size());
+        }
 
+    @Test
+    public void shouldAddBookToLibrary(){
+        Book testBook = new Book("100 años de soledad", "Gabriel Garcia", "1000");
+        Library library = new Library();
+        library.addBook(testBook);
+        assertEquals(testBook, library.books().get(0));
+    }
 
 
 }
